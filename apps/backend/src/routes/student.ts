13b92d2f7eacac1c22b87c2bcee2a839f7694036
Ayush Acharjya <ayushacharjya@gmail.com>
2024-12-03 23:47:34 +0000

    add student route to get all courses

--- apps/backend/src/routes/student.ts
+++ apps/backend/src/routes/student.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { authMiddleware } from '../middlewares/authMiddleware';
+import { getCourses } from '../controllers/studentController';
 
 const router = Router();
+
+router.get('/courses', authMiddleware, getCourses);
 
 export const studentRouter = router;
