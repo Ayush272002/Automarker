9bf764aff50c0e4dc2b0d94eb6410ec78c3fa8c5
highjeans <vivan.waghela23@gmail.com>
2024-12-10 16:14:17 +0000

    Remove unused components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,10 +1,8 @@
 import '../styles/globals.css';
 
-export * from './components/ui/avatar';
 export * from './components/ui/button';
 export * from './components/ui/card';
 export * from './components/ui/dialog';
 export * from './components/ui/input';
 export * from './components/ui/label';
 export * from './components/ui/progress';
-export * from './components/ui/select';
