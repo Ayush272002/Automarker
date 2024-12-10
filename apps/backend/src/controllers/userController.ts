268c77c1f65d1be8f5e05865622cd9780a670010
highjeans <vivan.waghela23@gmail.com>
2024-12-10 00:33:48 +0000

    Create /users/user GET request

--- apps/backend/src/controllers/userController.ts
+++ apps/backend/src/controllers/userController.ts
@@ -208,4 +208,35 @@ const getAllCourses = async (req: Request, res: Response) => {
   }
 };
 
-export { createUser, signin, logout, getAllCourses };
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.body.userId;
+    const user = await prisma.user.findFirst({
+      select: {
+        email: true,
+        firstName: true,
+        lastName: true,
+        role: true,
+      },
+      where: {
+        id: userId,
+      },
+    });
+
+    console.log(user);
+
+    if (!user) {
+      return res.status(403).json({
+        message: 'User not found',
+      });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: 'Internal server error',
+    });
+  }
+};
+
+export { createUser, signin, logout, getAllCourses, getUser };
